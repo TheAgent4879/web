103aedc2edfe135424fb3f35b9be6f5902cf4316
The Agent <144707881+TheAgent4879@users.noreply.github.com>
2023-12-08 21:16:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-// You can add more JavaScript logic here if needed
-// For now, let's just prevent any user interaction
+document.addEventListener('DOMContentLoaded', function () {
+    const maintenanceTime = getMaintenanceTime();
+    const errorMessage = document.getElementById('error-message');
+    const maintenanceInfo = document.getElementById('maintenance-info');
 
-document.addEventListener('click', function (event) {
-    event.preventDefault();
+    if (maintenanceTime) {
+        errorMessage.innerText = `Scheduled maintenance. Please check back after ${maintenanceTime}.`;
+        maintenanceInfo.innerText = `Maintenance scheduled at: ${maintenanceTime}`;
+    } else {
+        errorMessage.innerText = 'Error: Access Denied';
+    }
 });
 
-document.addEventListener('contextmenu', function (event) {
-    event.preventDefault();
-});
+function retry() {
+    // You can implement logic to retry or redirect the user
+    window.location.reload();
+}
+
+function getMaintenanceTime() {
+    // Implement logic to get maintenance time dynamically
+    // For example, fetch it from a server or calculate based on a schedule
+    return '01:00 PM';
+}
