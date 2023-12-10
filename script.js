d3d8581bce0b580e446c750ff54af076c7e78f1a
The Agent <144707881+TheAgent4879@users.noreply.github.com>
2023-12-10 16:50:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 document.addEventListener('DOMContentLoaded', function () {
     const errorMessage = document.getElementById('error-message');
+    const maintenanceTime = getMaintenanceTime();
 
-    if (maintenanceTime) {
-        errorMessage.innerText = 'Scheduled maintenance. Please try again later.';
+    // Check if maintenanceTime is defined
+    if (maintenanceTime !== undefined && maintenanceTime !== null) {
+        errorMessage.innerText = `Scheduled maintenance. Please check back after ${maintenanceTime}.`;
     } else {
         errorMessage.innerText = 'Error: Access Denied';
     }
+
+    // You can add more JavaScript logic here if needed
 });
 
 function retry() {
     // You can implement logic to retry or redirect the user
     window.location.reload();
+}
+
+// Function to get maintenance time (you can implement this as per your requirements)
+function getMaintenanceTime() {
+    // Replace this with your logic to retrieve maintenance time
+    // For example, you can use localStorage or make a server request
+    return localStorage.getItem('maintenanceTime');
 }
