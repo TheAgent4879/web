5047189c36b7f6bab3c92de530992025fddc6fa1
The Agent <144707881+TheAgent4879@users.noreply.github.com>
2023-12-19 20:04:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const cookieBanner = document.getElementById('cookieBanner');
-
-    // Check if the user has already accepted cookies
-    const cookiesAccepted = localStorage.getItem('cookiesAccepted');
-
-    if (!cookiesAccepted) {
-        // Show the cookie banner if not accepted
-        cookieBanner.style.display = 'block';
-    }
-});
-
-function acceptCookies() {
-    // Set a flag in localStorage to indicate that cookies are accepted
-    localStorage.setItem('cookiesAccepted', 'true');
-
-    // Hide the cookie banner
-    document.getElementById('cookieBanner').style.display = 'none';
-}
-
 function retry() {
     // You can implement logic to retry or redirect the user
     window.location.reload();
